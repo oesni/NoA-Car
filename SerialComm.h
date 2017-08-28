bb0b5a2d8803831d9d31378d743eebcd219e866f
Taehoooon-Kim <nbpryun@gmail.com>
2017-08-28 11:05:55 +0900

    Create SerialComm.h

---
+++ SerialComm.h
@@ -0,0 +1,27 @@
+#ifndef SERIALCOMM_H
+#define SERIALCOMM_H
+
+#include "SerialPort.h"
+
+#define RETURN_SUCCESS 1
+#define RETURN_FAIL 0
+
+
+class CSerialComm
+{
+public:
+	CSerialComm();
+	~CSerialComm();
+
+	CSerialPort	serial;
+	int		connect(char* _portNum);
+	int		sendCommand(char pos);
+	int recvCommand(char* &pos);
+	int recvCommand(char &one);
+	void		disconnect();
+
+	int sendCommandbyBluetooth(char data);
+	int recvCommandbyBluetooth(char &data);
+};
+
+#endif
